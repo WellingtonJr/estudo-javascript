ec14db06af7e95d45a137394e25de912fe01f0c5
WellingtonJr <wferreira.bm13@gmail.com>
2022-06-28 21:46:01 -0300

    to-do list

--- assets/js/to-do-list.js
+++ assets/js/to-do-list.js
@@ -1,1 +1,32 @@
- 
+var adicionarWrapper = document.getElementById('adicionar');
+
+adicionarWrapper.addEventListener('click',adicionaTarefa);
+
+var divTarefas = document.getElementById('lista-tarefas');
+
+var contador = 0;
+
+function adicionaTarefa(){
+    var tarefa = document.getElementById("tarefa");
+   
+
+    var inputTarefa = document.createElement('input');
+    inputTarefa.setAttribute('type','checkbox');
+    inputTarefa.setAttribute('id',contador);
+
+    var labelInputTarefa = document.createElement('label');
+    labelInputTarefa.setAttribute('for',contador);
+    labelInputTarefa.setAttribute('class','label-input-tarefa')
+    labelInputTarefa.innerText = tarefa.value;
+    contador++;
+
+    var divNovaTarefa = document.createElement('div');
+    divNovaTarefa.setAttribute('class','tarefa-checkbox');
+
+    divNovaTarefa.appendChild(inputTarefa);
+    divNovaTarefa.appendChild(labelInputTarefa);
+
+    divTarefas.appendChild(divNovaTarefa);
+    
+}
+
